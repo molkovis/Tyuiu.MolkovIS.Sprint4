1069dd3e2e28c076cd77c329158335d17f93ba2f
MolkovIS <Tyuiu.MolkovIS@outlook.com>
2025-10-20 18:14:13 +0500

    Add project files.

---
+++ Tyuiu.MolkovIS.Sprint4.Task0.V19.Lib/DataService.cs
@@ -0,0 +1,20 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+
+namespace Tyuiu.MolkovIS.Sprint4.Task0.V19.Lib
+{
+    public class DataService : ISprint4Task0V19
+    {
+        public int GetMultOddArrEl(int[] array)
+        {
+            int proiz = 1;
+            for (int i = 0; i <= array.Length; i++)
+            {
+                if (array[i] % 2 != 0)
+                {
+                    proiz = proiz * array[i];
+                }
+            }
+            return proiz;
+        }
+    }
+}
---
+++ Tyuiu.MolkovIS.Sprint4.Task0.V19/Program.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.MolkovIS.Sprint4.Task0.V19.Lib;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService dataService = new DataService();
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+        int[] array = {1,2,3,4,5,6,7,8,9,10};
+        
+        int result = dataService.GetMultOddArrEl(array);
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine(result);
+        Console.ReadKey();
+    }
+}
