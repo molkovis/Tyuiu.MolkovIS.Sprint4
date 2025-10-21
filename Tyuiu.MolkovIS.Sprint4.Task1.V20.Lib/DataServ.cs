57fb5f1d0c4f825dcf2ea71b4aab9741dfcd5861
MolkovIS <Tyuiu.MolkovIS@outlook.com>
2025-10-21 20:43:49 +0500

    Task1

---
+++ Tyuiu.MolkovIS.Sprint4.Task1.V20.Lib/DataServ.cs
@@ -0,0 +1,20 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+
+namespace Tyuiu.MolkovIS.Sprint4.V20.Lib
+{
+    public class DataService : ISprint4Task1V20
+    {
+        public int Calculate(int[] array)
+        {
+            int sum = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] % 2 != 0)
+                {
+                    sum += array[i];
+                }
+            }
+            return sum;
+        }
+    }
+}
