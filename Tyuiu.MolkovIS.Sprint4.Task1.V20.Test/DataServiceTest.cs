579ca44640f140606f824a0eba4e149f1f9056ea
MolkovIS <Tyuiu.MolkovIS@outlook.com>
2025-10-21 20:41:51 +0500

    Task1

---
+++ Tyuiu.MolkovIS.Sprint4.Task1.V20.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu.MolkovIS.Sprint4.Task1.V20.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Tyuiu.MolkovIS.Sprint4.Task1.V20/Program.cs
@@ -0,0 +1,25 @@
+﻿using Tyuiu.MolkovIS.Sprint4.V20.Lib;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService dataService = new DataService();
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+        int[] array = new int[10];
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = Convert.ToInt32(Console.ReadLine());
+        }
+
+
+        int result = dataService.Calculate(array);
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine(result);
+        Console.ReadKey();
+    }
+}
