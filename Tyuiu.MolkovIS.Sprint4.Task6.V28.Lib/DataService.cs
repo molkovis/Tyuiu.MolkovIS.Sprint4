5509659d5f7f0cc9d43fd678e72966ae5cf1d7f3
MolkovIS <Tyuiu.MolkovIS@outlook.com>
2025-10-28 12:05:00 +0500

    Task6

---
+++ Tyuiu.MolkovIS.Sprint4.Task6.V28.Lib/DataService.cs
@@ -0,0 +1,13 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+
+namespace Tyuiu.MolkovIS.Sprint4.Task6.V28.Lib
+{
+    public class DataService : ISprint4Task6V28
+    {
+        public string[] Calculate(string[] array)
+        {
+            string[] mas = Array.FindAll(array, x => x.Length == 4);
+            return mas;
+        }
+    }
+}
---
+++ Tyuiu.MolkovIS.Sprint4.Task6.V28.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu.MolkovIS.Sprint4.Task6.V28.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
