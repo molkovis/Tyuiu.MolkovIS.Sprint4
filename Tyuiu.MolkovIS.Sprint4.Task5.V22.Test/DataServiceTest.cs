46be30e732ba02825fa373fff23114a93ef908c5
MolkovIS <Tyuiu.MolkovIS@outlook.com>
2025-10-28 11:48:27 +0500

    task5

---
+++ Tyuiu.MolkovIS.Sprint4.Task5.V22.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu.MolkovIS.Sprint4.Task5.V22.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Tyuiu.MolkovIS.Sprint4.Task5.V22/Program.cs
@@ -0,0 +1,41 @@
+﻿using Tyuiu.MolkovIS.Sprint4.Task5.V22.Lib;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService dataService = new DataService();
+        Random rand = new Random();
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("Введите кол-во строк");
+        int row = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите кол-во столбцов");
+        int col = Convert.ToInt32(Console.ReadLine());
+
+        int[,] data = new int[row, col]; 
+
+        for (int i = 0; i < row; i++)
+        {
+            for (int j = 0; j < col; j++)
+            {
+                data[i, j] = rand.Next(-8,9);
+            }
+        }
+        Console.WriteLine("***************************************************************************");
+        for (int i = 0; i < row; i++)
+        {
+            for (int j = 0; j < col; j++)
+            {
+                Console.Write($"{data[i, j]}\t");
+            }
+        }
+        Console.WriteLine();
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine(dataService.Calculate(data));
+        Console.ReadKey();
+    }
+}
